95e6a3cc5c59a2236309634071908c3ee0b0a685
Kaio Vinícios <kvsgpro@outlook.com>
2024-07-12 17:03:43 -0300

    feat: creating initial draft login and registration page.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.accounts.views import footer
+from apps.accounts.views import footer, login, register
 
 urlpatterns = [
-   path('', footer)
+   path('', footer),
+   path('login/', login),
+   path('register/', register)
 ]
