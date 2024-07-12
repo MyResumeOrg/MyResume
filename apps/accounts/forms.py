bc91d93143ddff9bf8f00097b4805121aa6a73a5
Kaio Vinícios <kvsgpro@outlook.com>
2024-07-12 17:02:52 -0300

    feat: creating login and registration forms for the accounts app.

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,1 +1,112 @@
 from django import forms
+
+class LoginForms(forms.Form):
+    login_email = forms.EmailField(
+        label= 'Email',
+        required= True,
+        max_length= 200,
+        widget= forms.TextInput(
+            attrs={
+                'class':'form_input',
+                'placeholder':'name@example.com'
+            }
+        )
+    )
+
+    password = forms.CharField(
+        label= 'Password',
+        required= True,
+        max_length= 100,
+        widget= forms.PasswordInput(
+            attrs={
+                'class':'form_input',
+                'placeholder':'Password'
+            }
+        )
+    )
+
+class RegisterForms(forms.Form):
+    first_name = forms.CharField(
+        label= 'First name',
+        required= True,
+        max_length= 70,
+        widget= forms.TextInput(
+            attrs = {
+                'class':'form_input',
+                'placeholder':'First name'
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        label= 'Last name',
+        required= True,
+        max_length= 70,
+        widget= forms.TextInput(
+            attrs = {
+                'class':'form_input',
+                'placeholder':'Last name'
+            }
+        )
+    )
+
+    birth_data = forms.DateField(
+        label= 'Date of birth',
+        required= True,
+        widget= forms.DateInput(
+            attrs= {
+                'class':'form_input',
+                'type':'date',
+                'placeholder':'MM/DD/YYYY'
+            }
+        )
+    )
+
+    username = forms.CharField(
+        label= 'Username',
+        required= True,
+        max_length= 100,
+        widget= forms.TextInput(
+            attrs= {
+                'class':'form_input',
+                'placeholder':'Username'
+            }
+        )
+    )
+
+    register_email = forms.EmailField(
+        label= 'Email',
+        required= True,
+        max_length= 200,
+        widget= forms.TextInput(
+            attrs= {
+                'class':'form_input',
+                'placeholder':'name@example.com'
+            }
+        )
+    )
+
+    password = forms.CharField(
+        label= 'Password',
+        required= True,
+        max_length= 100,
+        widget= forms.PasswordInput(
+            attrs={
+                'class':'form_input',
+                'placeholder':'Password'
+            }
+        )
+    )
+
+    password_confirmation = forms.CharField(
+        label= 'Password confirmation',
+        required= True,
+        max_length= 100,
+        widget= forms.PasswordInput(
+            attrs= {
+                'class':'form_input',
+                'placeholder':'Password confirmation'
+            }
+        )
+    )
+
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render, redirect
 # from apps.accounts.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from apps.accounts.forms import LoginForms, RegisterForms
 from django.contrib import messages
 
 def footer(request):
     return render(request, 'partials/footer.html')
+
+def login(request):
+    form = LoginForms()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def register(request):
+    form = RegisterForms()
+    return render(request, 'accounts/register.html', {'form': form})
