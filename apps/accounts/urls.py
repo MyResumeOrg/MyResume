638cceadb65b7217e3a892a727d59d5f901e8077
Kaio Vinícios <kvsgpro@outlook.com>
2024-10-21 11:07:53 -0300

    feat: add url profile_page

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.accounts.views import login, register, terms_of_use
+from apps.accounts.views import login, register, terms_of_use, profile_page
 
 urlpatterns = [
    path('login/', login, name='login'),
    path('register/', register, name='register'),
-   path('terms_of_use/', terms_of_use, name='terms_of_use')
+   path('terms_of_use/', terms_of_use, name='terms_of_use'),
+   path('profile/', profile_page, name='profile_page')
 ]
