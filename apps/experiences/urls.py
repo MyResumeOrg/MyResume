60a4bdb5894d3f208ce1c2aa6ce938b7b22bf44d
Kaio Vinícios <kvsgpro@outlook.com>
2024-11-15 21:26:53 -0300

    feat: create building view and url paths

--- apps/experiences/urls.py
+++ apps/experiences/urls.py
@@ -1,7 +1,8 @@
 # from django.contrib import admin
 from django.urls import path
-from apps.experiences.views import add_experiences
+from apps.experiences.views import add_experiences, my_experiences
 
 urlpatterns = [
-   path('add_experiences/', add_experiences, name='add_experiences')
+   path('add_experiences/', add_experiences, name='add_experiences'),
+   path('my_experiences/', my_experiences, name='my_experiences'),
 ]
--- apps/resumes/urls.py
+++ apps/resumes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from apps.resumes.views import resume_v1
+from apps.core.views import building
 
 urlpatterns = [
-   path('basic/<str:username>/', resume_v1, name='resume'),
+   path('basic/<str:username>/', resume_v1, name='basic'),
+   path('complete/', building, name='complete')
 ]
