f95f9394d6e0f6c3d4ff3ecbc25d7441ce676556
Kaio Vinícios <kvsgpro@outlook.com>
2024-11-27 18:14:39 -0300

    feat: add is_owner verification in building view

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,4 +4,5 @@ def index(request):
     return render(request, 'core/home.html')
 
 def building(request):
-    return render(request, 'core/building.html')
+    is_owner = True if request.user.is_authenticated else False
+    return render(request, 'core/building.html', {'is_owner' : is_owner})
