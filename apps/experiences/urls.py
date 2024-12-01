07a4e88041a1de08837851868468a950864832d6
Kaio Vinícios <kvsgpro@outlook.com>
2024-12-01 19:22:31 -0300

    feat: create delete_experiences URL

--- apps/experiences/urls.py
+++ apps/experiences/urls.py
@@ -1,8 +1,9 @@
 # from django.contrib import admin
 from django.urls import path
-from apps.experiences.views import add_experiences, my_experiences
+from apps.experiences.views import add_experiences, my_experiences, delete_experiences
 
 urlpatterns = [
    path('add_experiences/', add_experiences, name='add_experiences'),
    path('my_experiences/', my_experiences, name='my_experiences'),
+   path('delete_experiences/<int:experience_id>/', delete_experiences, name='delete_experiences')
 ]
