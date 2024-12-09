dd6bd25be1c47b20705d9517bd7e7563ab149e1f
Kaio Vinícios <kvsgpro@outlook.com>
2024-12-08 21:41:41 -0300

    feat: add DOM events to global.js

--- setup/static/js/global.js
+++ setup/static/js/global.js
@@ -12,5 +12,23 @@ function removeElementTimer(idOrClass, seconds){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('hard_skill').classList.remove('none');
     removeElementTimer('.message_div', 5);
+
+    const radios = document.querySelectorAll('input[type="radio"]');
+    let activeContent = document.querySelector('input[type="radio"]:checked').value;
+
+    function updateContent() {
+        document.getElementById(activeContent).classList.add('none');
+        activeContent = this.value;
+        document.getElementById(activeContent).classList.remove('none');
+    }
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', updateContent);
+    });
+});
+
+document.getElementById('button_add_file').addEventListener('click', () => {
+    document.getElementsByClassName('file_input')[0].click();
 });
